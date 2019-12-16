f3f0dbcf5bfe509a66a94d3f7cd29eb7ff880c1c
Ruslan <ruslan.kovshar@gmail.com>
2019-12-17 00:21:02 +0200

    update server, fix client

--- src/main/java/ruslan/kovshar/client/UserWindow.java
+++ src/main/java/ruslan/kovshar/client/UserWindow.java
@@ -1,25 +1,14 @@
 package ruslan.kovshar.client;
 
-import com.sun.javafx.property.adapter.PropertyDescriptor;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.adapter.JavaBeanObjectProperty;
-import javafx.beans.property.adapter.JavaBeanObjectPropertyBuilder;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -27,16 +16,14 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
-import org.w3c.dom.ls.LSOutput;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class UserWindow extends Application {
@@ -44,6 +31,17 @@ public class UserWindow extends Application {
     private Socket socket;
     private Scanner scanner;
     private PrintWriter printWriter;
+    private VBox vBox;
+    private ScrollPane scroll;
+    private TextField textField;
+
+    public VBox getvBox() {
+        return vBox;
+    }
+
+    public void setvBox(VBox vBox) {
+        this.vBox = vBox;
+    }
 
     public Socket getSocket() {
         return socket;
@@ -72,7 +70,7 @@ public class UserWindow extends Application {
     public UserWindow() {
         try {
             socket = new Socket();
-            socket.connect(new InetSocketAddress("localhost", 8189), 2000);
+            socket.connect(new InetSocketAddress("77.47.221.76", 8189), 2000);
             scanner = new Scanner(socket.getInputStream());
             printWriter = new PrintWriter(socket.getOutputStream(), true);
             System.out.println(scanner.hashCode());
@@ -95,7 +93,7 @@ public class UserWindow extends Application {
     }
 
     private void sendMessage(UserWindowController userWindowController) {
-        String text = userWindowController.getTextField().getText().trim();
+        String text = textField.getText().trim();
         System.err.println(scanner.hashCode());
         System.err.println(printWriter);
         printWriter.println(text);
@@ -112,42 +110,39 @@ public class UserWindow extends Application {
         FXMLLoader loader = new FXMLLoader(location);
         Pane root = loader.load();
         UserWindowController userWindowController = loader.getController();
+
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("s1mpleChat");
         primaryStage.show();
+        textField = userWindowController.getTextField();
+        textField.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode().getName().equals("Enter")) {
+                sendMessage(userWindowController);
+            }
+        });
 
         userWindowController.getSendBtn()
-                .setOnAction(actionEvent -> sendMessage(userWindowController));
+                .setOnAction(actionEvent -> {
+                    sendMessage(userWindowController);
+                    textField.requestFocus();
+                });
 
+        scroll = userWindowController.getScroll();
 
-        VBox vbox = userWindowController.getvBox();
-        ObjectProperty<VBox> tfp = new SimpleObjectProperty<>(vbox);
+        vBox = userWindowController.getvBox();
+        scroll.vvalueProperty().bind(vBox.heightProperty());
+        ObjectProperty<VBox> tfp = new SimpleObjectProperty<>(vBox);
 
-        /*tfp.addListener((obs, oldVal, newVal) -> {
-            System.out.println(obs + " " + oldVal + "->" + newVal);
-            ///obs.getValue().getChildren().forEach(System.out::println);
-
-            //oldVal.getChildren().forEach(System.out::println);
-            newVal.getChildren().forEach(System.out::println);
-
-            //ObservableList<Node> oldValChildren = oldVal.getChildren();
-            //ObservableList<Node> newValChildren = newVal.getChildren();
-            //obs.getValue().getChildren().addAll(oldValChildren);
-            //obs.getValue().getChildren().addAll(newValChildren);
-        });*/
-        tfp.addListener((observableValue, textFlow1, t1) -> Platform.runLater(() -> {
-            textFlow1.getChildren().add(t1);
-            //userWindowController.getScroll().setVvalue(1.0);
-        }));
+        tfp.addListener((observableValue, vBoxx, t1) -> Platform.runLater(() -> getvBox().getChildren().addAll(t1.getChildren())));
         MyService service = new MyService(scanner, tfp);
         service.start();
-
-
-        System.out.println("End start");
     }
 
+    public void update(Text text) {
+        Platform.runLater(() -> vBox.getChildren().add(text));
+    }
 
-    static class MyService extends Service<String> {
+    class MyService extends Service<String> {
         private Scanner scanner;
         ObjectProperty<VBox> tfp = new SimpleObjectProperty<>();
 
@@ -165,15 +160,13 @@ public class UserWindow extends Application {
                         while (true) {
                             if (scanner.hasNextLine()) {
                                 String s = scanner.nextLine();
-                                System.out.println(s + "!!!!!");
-
-
-                                Text text = new Text(s);
-                                text.setFont(new Font(20));
-                                text.setWrappingWidth(200);
+                                byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+                                String utf8String = new String(bytes);
+                                Text text = new Text(utf8String);
+                                text.setFont(new Font(14));
+                                text.setWrappingWidth(vBox.widthProperty().intValue());
                                 text.setTextAlignment(TextAlignment.JUSTIFY);
-                                VBox vBox = new VBox(text);
-                                tfp.set(vBox);
+                                update(text);
                             }
                         }
                     } catch (Exception e) {
--- src/main/java/ruslan/kovshar/server/SimpleServerSocket.java
+++ src/main/java/ruslan/kovshar/server/SimpleServerSocket.java
@@ -4,46 +4,78 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SimpleServerSocket {
+
+    private static List<MySocket> clients = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
             System.out.println("Server start...");
             while (true) {
                 Socket socket = serverSocket.accept();
-                new Thread(new MySocket(socket)).start();
+
+                MySocket target = new MySocket(socket);
+                clients.add(target);
+                new Thread(target).start();
             }
+        }
+    }
+
+    public static void removeClient(MySocket target) {
+        clients.remove(target);
+    }
+
+    public static void sendToAll(String message) {
+        for (MySocket target :
+                clients) {
+            target.sendMessage(message);
         }
     }
 }
 
 class MySocket implements Runnable {
     private Socket socket;
+    private Scanner scanner;
+    private PrintWriter writer;
 
     public MySocket(Socket socket) {
-        this.socket = socket;
+        try {
+            this.socket = socket;
+            this.scanner = new Scanner(socket.getInputStream());
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
-        try (Scanner scanner = new Scanner(socket.getInputStream());
-             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+        try {
             while (true) {
                 if (scanner.hasNextLine()) {
                     String string = scanner.nextLine();
                     if (string.equalsIgnoreCase("#exit#")) {
                         System.err.println(string);
+                        SimpleServerSocket.removeClient(this);
                         break;
                     }
-                    writer.println("Your message: " + string);
+                    SimpleServerSocket.sendToAll(string);
+                    //writer.println("Your message: " + string);
                     System.out.println(string);
                 }
                 Thread.sleep(100);
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendMessage(String message) {
+        writer.println(message);
     }
 }
 
